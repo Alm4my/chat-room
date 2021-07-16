1ac2a189570b6b3bdad77c78da2f7c1e1e06a397
Alm4my <thenixadm@gmail.com>
2021-07-16 21:43:12 +0000

    Add methods stubs

--- src/main/java/fr/almamy/chatroom/config/WebSocketConfig.java
+++ src/main/java/fr/almamy/chatroom/config/WebSocketConfig.java
@@ -1,4 +1,42 @@
 package fr.almamy.chatroom.config;
 
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/chat")
 public class WebSocketConfig {
+    /// === Fields ===
+    /* All Chat Sessions */
+    private Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+
+    /// === Private Methods ===
+    private static void sendMessageToAll(String msg){
+
+    }
+
+    /// === Public Methods ===
+    @OnOpen
+    public void onOpen(Session session){
+
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String jsonStr){
+
+    }
+
+    @OnClose
+    public void onClose(Session session){
+
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error){
+
+    }
 }
--- src/main/java/fr/almamy/chatroom/controller/WebSocketChatServer.java
+++ src/main/java/fr/almamy/chatroom/controller/WebSocketChatServer.java
@@ -1,5 +1,13 @@
 package fr.almamy.chatroom.controller;
 
-public class WebSocketChatServer {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+@Configuration
+public class WebSocketChatServer {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
