cd8500f2fc9cccd70f2c4ae8c81a8cf312e08714
Alm4my <thenixadm@gmail.com>
2021-07-16 21:53:59 +0000

    Fix content of both classes

--- src/main/java/fr/almamy/chatroom/controller/WebSocketChatServer.java
+++ src/main/java/fr/almamy/chatroom/controller/WebSocketChatServer.java
@@ -1,13 +1,42 @@
 package fr.almamy.chatroom.controller;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.stereotype.Component;
 
-@Configuration
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/chat")
 public class WebSocketChatServer {
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
+    /// === Fields ===
+    /* All Chat Sessions */
+    private Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+
+    /// === Private Methods ===
+    private static void sendMessageToAll(String msg){
+
+    }
+
+    /// === Public Methods ===
+    @OnOpen
+    public void onOpen(Session session){
+
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String jsonStr){
+
+    }
+
+    @OnClose
+    public void onClose(Session session){
+
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error){
+
     }
 }
