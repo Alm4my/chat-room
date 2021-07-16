c732f18ef8bff973f701eeece7e42ecf1d6692bc
Alm4my <thenixadm@gmail.com>
2021-07-16 22:07:30 +0000

    Implement Message

--- src/main/java/fr/almamy/chatroom/model/Message.java
+++ src/main/java/fr/almamy/chatroom/model/Message.java
@@ -1,5 +1,6 @@
 package fr.almamy.chatroom.model;
 
+import com.alibaba.fastjson.JSON;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,19 @@ import lombok.Setter;
 @Setter @Getter
 @AllArgsConstructor @NoArgsConstructor
 public class Message {
+    /// === Constants ===
+    @Getter public static final String ENTER = "ENTER";
+    @Getter public static final String SPEAK = "SPEAK";
+    @Getter public static final String QUIT = "QUIT";
+
     /// === Fields ===
-    private String content;
+    private String type;
+    private String username;
+    private String msg;
+    private int onlineCount;
+
+    /// === Public Methods ===
+    public static String jsonStr(String type, String username, String msg, int onlineTotal) {
+        return JSON.toJSONString(new Message(type, username, msg, onlineTotal));
+    }
 }
