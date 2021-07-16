9fcba28cd085cbfffc17a5460b0e388ca3b16a30
Alm4my <thenixadm@gmail.com>
2021-07-16 21:25:45 +0000

    Add Message model

---
+++ src/main/java/fr/almamy/chatroom/model/Message.java
@@ -0,0 +1,13 @@
+package fr.almamy.chatroom.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor @NoArgsConstructor
+public class Message {
+    /// === Fields ===
+    private String content;
+}
