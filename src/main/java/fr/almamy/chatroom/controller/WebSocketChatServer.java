9489549886db2a9f7b72cf0333bc839401b3ac00
Alm4my <thenixadm@gmail.com>
2021-07-16 22:05:34 +0000

    Implement chat server

--- src/main/java/fr/almamy/chatroom/controller/WebSocketChatServer.java
+++ src/main/java/fr/almamy/chatroom/controller/WebSocketChatServer.java
@@ -1,9 +1,12 @@
 package fr.almamy.chatroom.controller;
 
+import com.alibaba.fastjson.JSON;
+import fr.almamy.chatroom.model.Message;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -12,31 +15,50 @@ import java.util.concurrent.ConcurrentHashMap;
 public class WebSocketChatServer {
     /// === Fields ===
     /* All Chat Sessions */
-    private Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+    private static final Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
 
     /// === Private Methods ===
     private static void sendMessageToAll(String msg){
-
+        onlineSessions.forEach((id, session) ->{
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        });
     }
 
     /// === Public Methods ===
     @OnOpen
     public void onOpen(Session session){
-
+        onlineSessions.put(session.getId(), session);
+        sendMessageToAll(Message.jsonStr("ENTER",
+                "",
+                "",
+                onlineSessions.size()));
     }
 
     @OnMessage
     public void onMessage(Session session, String jsonStr){
+        var message = (Message) JSON.parseObject(jsonStr, Message.class);
+        sendMessageToAll(Message.jsonStr("SPEAK",
+                message.getUsername(),
+                message.getMsg(),
+                onlineSessions.size()));
 
     }
 
     @OnClose
     public void onClose(Session session){
-
+        onlineSessions.remove(session.getId());
+        sendMessageToAll(Message.jsonStr("QUIT",
+                "",
+                "",
+                onlineSessions.size()));
     }
 
     @OnError
     public void onError(Session session, Throwable error){
-
+        error.printStackTrace();
     }
 }
